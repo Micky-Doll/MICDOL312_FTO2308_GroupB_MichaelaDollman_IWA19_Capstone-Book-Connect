7dec2ebf5870432122d4ddf86e9d47c802146ece
Micky-Doll <michaelaleedollman@gmail.com>
2023-11-24 16:39:23 +0200

    Finalising comments and JSdocs

--- scripts.js
+++ scripts.js
@@ -1,130 +1,3 @@
-// matches = books
-// page = 1;
-
-// if (!books && !Array.isArray(books)) throw new Error('Source required')
-
-// day = {
-//     dark: '10, 10, 20',
-//     light: '255, 255, 255',
-// }
-
-// night = {
-//     dark: '255, 255, 255',
-//     light: '10, 10, 20',
-// }
-
-// documentElement.style.setProperty('--color-dark', css[v].dark);
-// documentElement.style.setProperty('--color-light', css[v].light);
-
-// data-search-cancel.click() { data-search-overlay.open === false }
-// data-settings-cancel.click() { querySelect(data-settings-overlay).open === false }
-// data-settings-form.submit() { actions.settings.submit }
-// data-list-close.click() { data-list-active.open === false }
-
-// data-list-button.click() {
-//     document.querySelector([data-list-items]).appendChild(createPreviewsFragment(matches, page x BOOKS_PER_PAGE, {page + 1} x BOOKS_PER_PAGE]))
-//     actions.list.updateRemaining()
-//     page = page + 1
-// }
-
-// data-header-search.click() {
-//     data-search-overlay.open === true ;
-//     data-search-title.focus();
-// }
-
-// data-search-form.click(filters) {
-//     preventDefault()
-//     const formData = new FormData(event.target)
-//     const filters = Object.fromEntries(formData)
-//     result = []
-
-//     for (book; booksList; i++) {
-//         titleMatch = filters.title.trim() = '' && book.title.toLowerCase().includes[filters.title.toLowerCase()]
-//         authorMatch = filters.author = 'any' || book.author === filters.author
-
-//         {
-//             genreMatch = filters.genre = 'any'
-//             for (genre; book.genres; i++) { if singleGenre = filters.genre { genreMatch === true }}}
-//         }
-
-//         if titleMatch && authorMatch && genreMatch => result.push(book)
-//     }
-
-//     if display.length < 1
-//     data-list-message.class.add('list__message_show')
-//     else data-list-message.class.remove('list__message_show')
-
-//     data-list-items.innerHTML = ''
-//     const fragment = document.createDocumentFragment()
-//     const extracted = source.slice(range[0], range[1])
-
-//     for ({ author, image, title, id }; extracted; i++) {
-//         const { author: authorId, id, image, title } = props
-
-//         element = document.createElement('button')
-//         element.classList = 'preview'
-//         element.setAttribute('data-preview', id)
-
-//         element.innerHTML = /* html */ `
-//             <img
-//                 class="preview__image"
-//                 src="${image}"
-//             />
-
-//             <div class="preview__info">
-//                 <h3 class="preview__title">${title}</h3>
-//                 <div class="preview__author">${authors[authorId]}</div>
-//             </div>
-//         `
-
-//         fragment.appendChild(element)
-//     }
-
-//     data-list-items.appendChild(fragments)
-//     initial === matches.length - [page * BOOKS_PER_PAGE]
-//     remaining === hasRemaining ? initial : 0
-//     data-list-button.disabled = initial > 0
-
-//     data-list-button.innerHTML = /* html */ `
-//         <span>Show more</span>
-//         <span class="list__remaining"> (${remaining})</span>
-//     `
-
-//     window.scrollTo({ top: 0, behavior: 'smooth' });
-//     data-search-overlay.open = false
-// }
-
-// data-settings-overlay.submit; {
-//     preventDefault()
-//     const formData = new FormData(event.target)
-//     const result = Object.fromEntries(formData)
-//     document.documentElement.style.setProperty('--color-dark', css[result.theme].dark);
-//     document.documentElement.style.setProperty('--color-light', css[result.theme].light);
-//     data-settings-overlay).open === false
-// }
-
-// data-list-items.click() {
-//     pathArray = Array.from(event.path || event.composedPath())
-//     active;
-
-//     for (node; pathArray; i++) {
-//         if active break;
-//         const previewId = node?.dataset?.preview
-
-//         for (const singleBook of books) {
-//             if (singleBook.id === id) active = singleBook
-//         }
-//     }
-
-//     if !active return
-//     data-list-active.open === true
-//     data-list-blur + data-list-image === active.image
-//     data-list-title === active.title
-
-//     data-list-subtitle === '${authors[active.author]} (${Date(active.published).year})'
-//     data-list-description === active.description
-// }
-
 import { books, BOOKS_PER_PAGE, genres, authors, state } from "./data.js";
 import { createBookHtml, html } from "./view.js";
 
@@ -143,19 +16,28 @@ const themeSetting = {
   },
 };
 
+// Show More Button
 const createButtonText = (array) => {
-  html.list.button.innerHTML = /* html */ `
-    <span>Show more</span>
-    <span class="list__remaining"> (${
-      array.length - Object.keys(state.booksLoaded).length
-    })</span>
+  if (array.length === 0) {
+    html.list.button.disabled = true;
+    html.list.button.innerHTML = `
+Show More <span class="list__remaining">(${0})</span>
 `;
-  // data-list-button.disabled = !(matches.length - [page * BOOKS_PER_PAGE] > 0)
+  } else {
+    html.list.button.disabled = false;
+    html.list.button.innerHTML = `
+    Show More <span class="list__remaining">(${
+      array.length - Object.keys(state.loaded).length
+    })</span>`;
+  }
 };
 
-// Making a fragment to house our data element and loop it; Giving the information to the createHTML function to run instructions on
-// Everything that needs to happen to create multiple previews
-// Giving the function an editable parameter (array) to create books depending on necessity
+/**
+ * Making a fragment to house our data element and loop it; Giving the information to the createHTML function to run instructions on
+ * Everything that needs to happen to create multiple previews
+ * Giving the function an editable parameter (array) to create books depending on necessity
+ */
+
 const createBookList = (array) => {
   const startPosition = (state.pageNumber - 1) * BOOKS_PER_PAGE;
   const endPosition = startPosition + BOOKS_PER_PAGE;
@@ -166,7 +48,6 @@ const createBookList = (array) => {
   for (let i = 0; i < extracted.length; i++) {
     const bookInfo = extracted[i];
     state.booksLoaded[bookInfo.id] = bookInfo;
-    // state.loaded[id] = {id, image, author, title, description, publish}
     const preview = createBookHtml(bookInfo); // preview = what createBookHtml returns
     fragment.appendChild(preview);
   }
@@ -224,6 +105,7 @@ createBookList(books);
 createAuthorOptions();
 createGenreOptions();
 
+// Checking and matching local browser preferences for theme
 if (
   window.matchMedia &&
   window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -240,6 +122,10 @@ const handleSearchToggle = (event) => {
 const handleSettingsToggle = (event) => {
   html.settingsOverlay.overlay.toggleAttribute("open");
 };
+
+/**
+ * Gets ID value from the click event and uses it to get the information to be used in the overlay
+ */
 
 const handleItemClick = (event) => {
   event.preventDefault();
@@ -281,6 +167,8 @@ const handleShowMore = () => {
   createBookList(books);
 };
 
+// Day/Night Mode Toggle
+
 const handleSettingsSubmit = (event) => {
   event.preventDefault();
   const theme = event.target[0].value;
@@ -306,11 +194,6 @@ const handleSettingsSubmit = (event) => {
 // Event Listeners
 html.headerButtons.search.addEventListener("click", handleSearchToggle);
 
-// html.searchOverlay.form.addEventListener(
-//   "submit",
-//   console.log("search the database for books meeting set criteria")
-// );
-
 html.searchOverlay.cancel.addEventListener("click", handleSearchToggle);
 
 html.headerButtons.settings.addEventListener("click", handleSettingsToggle);
@@ -321,7 +204,3 @@ html.overlay.close.addEventListener("click", handleItemClick);
 html.list.button.addEventListener("click", handleShowMore);
 
 html.settingsOverlay.form.addEventListener("submit", handleSettingsSubmit);
-html.settingsOverlay.theme.addEventListener(
-  "click",
-  console.log("Opens dropdown menu to choose between day or night mode")
-);
